f55104289e307d91018177638850c81f097e651f
Sam Cooke <sammycooke2017@gmail.com>
2019-02-12 19:43:22 -0800

    second commit

--- sudoku.cpp
+++ sudoku.cpp
@@ -8,11 +8,33 @@ Sudoku::Sudoku() {
 
 }
 
-Sudoku:: ~Sudoku() {
+Sudoku::~Sudoku() {
 	for (int i = 0; i < SIZE; i++)
 		delete[] grid[i];
 
 	delete[] grid;
+}
+
+
+void Sudoku::genPuzzleFromFile(char *fname) {
+	ifstream inp; 
+	
+	inp.open(fname);
+}
+
+void Sudoku::printPuzzle() {
+	cout << "---------\n";
+
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			cout << "1" << grid[i][i];
+		}
+		cout << "---------" << endl;
+	}
+}
+
+const bool Sudoku::isValidRow(int x, int y, int v) {
+
 }
 
 	
