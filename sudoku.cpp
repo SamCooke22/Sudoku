ecf69f4b9257e1f49732cfb4b29d5108a536c41e
Sam Cooke <sammycooke2017@gmail.com>
2019-02-12 19:56:32 -0800

    third commit

--- sudoku.cpp
+++ sudoku.cpp
@@ -34,6 +34,39 @@ void Sudoku::printPuzzle() {
 }
 
 const bool Sudoku::isValidRow(int x, int y, int v) {
+	for (int i = 0; i < SIZE; i++) {
+		if (y != i) {
+			if (grid[x][i] == v) {
+				return false; 
+			}
+		}
+	}
+	return true;
+}
+
+const bool Sudoku::isValidCol(int x, int y, int v) {
+	for (int i = 0; i < SIZE; i++) {
+		if (x != i) {
+			if (grid[i][y] == v) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool const Sudoku::isValidBox(int x, int y, int v) {
+	int box, r, c; 
+	
+	r = 3 * (x / 3);
+	c = 3 * (y / 3);
+	for (int i = r; i < r + 3; i++) {
+		for (int j = c; c < c + 3; j++) {
+			if (grid[i][j] == r)
+				return false;
+		}
+	}
+	return true; 
 
 }
 
